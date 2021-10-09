0bc35104676474c94690d56aa6b7b0427dd61855
Taylor Monacelli <taylormonacelli@gmail.com>
2021-10-09 07:57:18 -0700

    real men dont make git commit comments

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+skaffold config set --global collect-metrics true
+git clone --depth 1 https://github.com/GoogleContainerTools/skaffold
+cd skaffold/examples/getting-started
+skaffold dev
