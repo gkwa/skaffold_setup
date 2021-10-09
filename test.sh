3802b22d171ab28cc75b4c26be80c7ddea6c3f4f
Taylor Monacelli <taylormonacelli@gmail.com>
2021-10-09 08:06:35 -0700

    real men dont make git commit comments

--- test.sh
+++ test.sh
@@ -3,4 +3,25 @@
 skaffold config set --global collect-metrics true
 git clone --depth 1 https://github.com/GoogleContainerTools/skaffold
 cd skaffold/examples/getting-started
+
+cat <<'__eot__' >main.go
+package main
+
+import (
+        "fmt"
+        "time"
+)
+
+func main() {
+	for i := 0; i < 10; i++ {
+                fmt.Println("Hello world!")
+                time.Sleep(time.Second * 1)
+        }
+}
+__eot__
+
+
+
+
+
 skaffold dev
